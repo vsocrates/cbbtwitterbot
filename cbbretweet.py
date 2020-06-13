a31d847fa7cc3345a7a67ba9dfbbcd4777cc651c
Vimig Socrates <vimig.socrates@gmail.com>
2020-06-13 08:07:50 -0500

    added dynamodb since_id check

--- cbbretweet.py
+++ cbbretweet.py
@@ -2,6 +2,8 @@ import os
 import json
 import time
 import tweepy
+import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
 
@@ -10,8 +12,21 @@ def lambda_handler(event, context):
   auth.set_access_token(os.environ['CBB_ACCESS_TOKEN'], os.environ['CBB_ACCESS_TOKEN_SECRET'])
   api = tweepy.API(auth)
 
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table("cbbtwitter")
+
+  since_id = 0
+  try:
+    response = table.get_item(Key={'id_param': "since_id"})
+  except ClientError as e:
+    print(e.response['Error']['Message'])
+  except KeyError as e:
+    print(e.response)
+  else:
+    since_id = response['Item']
+
   # TODO: implement support for extended tweets (240 chars)
-  for mention in tweepy.Cursor(api.mentions_timeline, include_rts = True).items():
+  for mention in tweepy.Cursor(api.mentions_timeline, include_rts = True, since_id=since_id).items():
     try:
       api.retweet(mention.id)
       time.sleep( 5 )
